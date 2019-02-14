26b634dd1bc579e157da0b47b3c677cf37e18cf2
何奔 <heben@jingyu.com>
2019-02-14 14:51:16 +0800

    Initial commit

---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'myHeader',
+    data () {
+        return {
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <el-container>
+        <el-header height="100px">
+
+        </el-header>
+        <el-main>
+            <el-button @click="handleClick">123</el-button>
+        </el-main>
+        <el-footer height="60px"></el-footer>
+    </el-container>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    data () {
+        return {
+        }
+    },
+    methods: {
+        handleClick () {
+            this.$axios({
+                url: 'http://localhost:3000/top/playlist/highquality',
+                params: {
+                    before: 1503639064232,
+                    limit: 3
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.el-container{
+
+    .el-header{
+        border-bottom 1px solid #f2f2f2
+    }
+}
+</style>
